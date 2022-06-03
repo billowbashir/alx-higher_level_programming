63af48d58fadd6b13caddc42f394c995c51a5fde
billowbashir <billowbashir@gmail.com>
2022-06-03 23:03:05 +0300

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
-# Python module to import
-
-print("File two __name__ is set to: {}" .format(__name__))
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-   print("File two executed when ran directly")
-else:
-   print("File two executed when imported")
+   from calculator_1 import add, sub, mul, div
+   a = 10
+   b = 5
+   print("{} + {} = {}".format(a, ,b, add(a, b)))
+   print("{} - {} = {}".format(a, ,b, sub(a, b)))
+   print("{} * {} = {}".format(a, ,b, mul(a, b)))
+   print("{} / {} = {}".format(a, ,b, div(a, b)))
