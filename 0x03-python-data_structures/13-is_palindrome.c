62b5d4d93a7e5c3c739c2919be3219475a797271
billowbashir <billowbashir@gmail.com>
2022-06-10 14:52:19 +0300

    0x0x4 delete

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,9 +7,9 @@
  */
 int is_palindrome(listint_t **head)
 {
-const listint_t *current;
-const listint_t *tail;
-unsigned int n = 0;
+    const listint_t *current;
+    const listint_t *tail;
+    unsigned int n = 0;
 
     //Get length
     //Use a function to get a node on a specific index
@@ -17,6 +17,5 @@ unsigned int n = 0;
     //return false if one check was False
     //return true at the end (if all passed the checks)
 
-
-return (n);
+    return (n);
 }
